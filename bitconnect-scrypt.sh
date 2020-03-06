3f70c045f7cc8315d013950254be32b0493dfee2
Jonny <38663528+JonathanHunterPringle@users.noreply.github.com>
2020-03-06 08:14:01 +0000

    Update bitconnect-scrypt.sh

--- bitconnect-scrypt.sh
+++ bitconnect-scrypt.sh
@@ -7,3 +7,5 @@ sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
 
 sudo apt-get install libdb4.8-dev libdb4.8++-dev libzmq3-dev libminiupnpc-dev libcrypto++-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev ufw git software-properties-common libtool autotools-dev autoconf pkg-config libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev automake g++-mingw-w64-x86-64 libevent-dev bsdmainutils curl git g++-mingw-w64-i686 mingw-w64-i686-dev g++-mingw-w64-x86-64 libdb5.3++-dev
+
+#!/bin/sh
